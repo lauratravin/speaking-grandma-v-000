d9af808e430d38de23cf7be2f9bcf06a597c93e8
Laura Messa Travin <laura.travin@gmail.com>
2018-03-21 01:32:01 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,4 +15,8 @@ def speak_to_grandma(phrase)
     if phrase != phrase.upcase
        return "HUH?! SPEAK UP, SONNY!"
     end
+    if phrase == phrase.upcase
+      return "NO, NOT SINCE 1938!"
+    end
+      
 end
